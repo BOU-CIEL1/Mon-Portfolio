02609b79a7aadf6412b92e736b6255d53d51ebd5
Jalil <jalilbougoffa@gmail.com>
2026-03-12 00:18:25 +0100

    Traduction de la page d'Accueil en Anglais et test Toggle Button Fr-En

--- docs/script.js
+++ docs/script.js
@@ -5,13 +5,38 @@
 4. Mettre du focus sur certains éléments
 */
 
-const toggleLightDark = document.getElementById('toggle-light-dark');
+//Light Mode - Dark Mode
+const toggleLightDark = document.getElementById("toggle-light-dark");
 const body = document.body;
 
-toggleLightDark.addEventListener('change', () => {
-    if(toggleLightDark.checked) {
-        document.body.classList.remove('light-mode');
-    } else {
-        document.body.classList.add('light-mode');
+toggleLightDark.addEventListener("change", () => {
+  if (toggleLightDark.checked) {
+    document.body.classList.remove("light-mode");
+  } else {
+    document.body.classList.add("light-mode");
+  }
+});
+
+//Français - Anglais
+const toggleFrEn = document.getElementById("toggle-fr-en");
+
+toggleFrEn.addEventListener("change", () => {
+  let currentPath = window.location.pathname;
+  let newPath = "";
+
+  if (toggleFrEn.checked) {
+    if (!currentPath.includes("/en/")) {
+      let fileName = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+      newPath = "./en/" + (fileName || "index.html");
     }
+  } else {
+    if (currentPath.includes("/en/")) {
+      newPath = "../" + currentPath.substring(currentPath.lastIndexOf("/") + 1);
+    }
+  }
+  if (newPath !== "") {
+    setTimeout(() => {
+      window.location.href = newPath;
+    }, 300);
+  }
 });
