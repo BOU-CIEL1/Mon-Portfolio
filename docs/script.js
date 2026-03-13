c7b7259d40c3053b042d41b14d39c31444a36e9c
Jalil <jalilbougoffa@gmail.com>
2026-03-13 18:17:09 +0100

    Définition des couleurs du dark et du light mode + localstorage pour changement de langue

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 /*TO DO LIST
-1. Faire un Toggle Button FR - EN
-2. Faire un Toggle Button Light Mode - Dark Mode
+1. Faire un Toggle Button FR - EN (traduire pages Formation, Exp, Certifs, Projets)
+2. Faire un Toggle Button Light Mode - Dark Mode (Changer les couleurs du Light Mode)
 3. Faire des animations rapides
 4. Mettre du focus sur certains éléments
 */
@@ -9,11 +9,18 @@
 const toggleLightDark = document.getElementById("toggle-light-dark");
 const body = document.body;
 
+if (localStorage.getItem('theme') === 'light') {
+    body.classList.add('light-mode');
+    toggleLightDark.checked = false;
+}
+
 toggleLightDark.addEventListener("change", () => {
   if (toggleLightDark.checked) {
     document.body.classList.remove("light-mode");
+    localStorage.setItem('theme', 'dark');
   } else {
     document.body.classList.add("light-mode");
+    localStorage.setItem('theme', 'light');
   }
 });
 
